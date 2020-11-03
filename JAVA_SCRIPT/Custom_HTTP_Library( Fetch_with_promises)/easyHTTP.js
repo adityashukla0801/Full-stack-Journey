15e2d62bb72c66a77332fdd0d36833f00aab86ed
Aditya shukla <64006149+adityashukla0801@users.noreply.github.com>
2020-11-03 12:53:34 +0530

    Add files via upload

---
+++ JAVA_SCRIPT/Custom_HTTP_Library( Fetch_with_promises)/easyHTTP.js
@@ -0,0 +1,62 @@
+class EasyHttp{
+
+  // Make HTTP Get Request
+  get(url){
+    return new Promise((resolve, reject) => {
+      fetch(url)
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err))
+    })
+  }
+
+
+ // Make HTTP Post Request
+ post(url, data){
+  return new Promise((resolve, reject) => {
+    fetch(url ,{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err))
+  })
+  }
+
+  // Make HTTP Put Request
+ put(url, data){
+  return new Promise((resolve, reject) => {
+    fetch(url ,{
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err))
+  })
+  }
+
+   // Make HTTP Delete Request
+ delete(url){
+  return new Promise((resolve, reject) => {
+    fetch(url ,{
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(() => resolve('Resourse deleted...'))
+    .catch(err => reject(err))
+  })
+  }
+
+
+}
